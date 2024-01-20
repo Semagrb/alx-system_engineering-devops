f6ef0f7cd2165e416ba14c8d0f58db062629d722
semagrb <fithisalma@gmail.com>
2024-01-19 16:57:41 -0800

    fixed error

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,25 +1,17 @@
-<<<<<<< HEAD
-#! /usr/bin/env puppet
-#  Ensure python3-pip is installed
+#!/usr/bin/env puppet
+
 package { 'python3-pip':
- ensure => installed,
+  ensure => installed,
 }
 
 package { 'flask':
- ensure   => '2.1.0',
- provider => pip3,
- require => Package['python3-pip'],
+  ensure   => '2.1.0',
+  provider => 'pip3',
+  require  => Package['python3-pip'],
 }
 
 package { 'werkzeug':
- ensure   => '2.0.1',
- provider => pip,
- require => Package['python3-pip'],
-=======
-#!/usr/bin/pup
-# Install an especific version of flask (2.1.0)
-package {'flask':
-  ensure   => '2.1.0',
-  provider => 'pip3'
->>>>>>> 4913a65a7734968cc129cf8c7d7d7c0f0411e5de
+  ensure   => '2.0.0',
+  provider => 'pip',
+  require  => Package['python3-pip'],
 }
